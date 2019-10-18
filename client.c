7f4078e615be11c014e4c67eb2101728ead86af5
Jared Hasson <jhasson@simplifile.com>
2019-10-18 15:16:05 -0600

    8. switch TCP to UDP

--- client.c
+++ client.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
 	hints.ai_flags = 0;
 	hints.ai_protocol = 0;          /* Any protocol */
 
