4ef879c556fd8971ad612600a924d70b325152ff
Jared Hasson <jhasson@simplifile.com>
2019-10-18 17:17:29 -0600

    change server to tcp

--- server.c
+++ server.c
@@ -23,8 +23,8 @@ int main(int argc, char *argv[]) {
 	}
 
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+	hints.ai_family = AF_INET;    /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
 	hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
 	hints.ai_protocol = 0;          /* Any protocol */
 	hints.ai_canonname = NULL;
